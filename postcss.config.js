0c18ad3674fafdffb2ebb78030aff4341ec378b6
Aaron Sumner <asumner@mac.com>
2020-09-16 22:00:49 -0700

    Attempt to optimize CSS in production only

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,14 @@ module.exports = {
   plugins: [
     require("tailwindcss"),
     require("autoprefixer"),
+    process.env.NODE_ENV === 'production' && require("@fullhuman/postcss-purgecss")({
+      content: [
+        "./_posts/*.markdown",
+        "./_layouts/*.html",
+        "./_includes/*.html",
+        "./*.html",
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ]
 }
